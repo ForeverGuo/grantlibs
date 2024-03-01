dcb2f28569d1db04714ad252893f9f78317a01fe
grantguo <810153274@qq.com>
2024-03-01 19:34:37 +0800

    deploy

---
+++ assets/guide_install.md.CHudu1Ue.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as i,o as s,a2 as t}from"./chunks/framework.2t550KrZ.js";const m=JSON.parse('{"title":"安装","description":"","frontmatter":{},"headers":[],"relativePath":"guide/install.md","filePath":"guide/install.md"}'),e={name:"guide/install.md"},n=t("",16),l=[n];function r(h,o,p,d,c,k){return s(),i("div",null,l)}const g=a(e,[["render",r]]);export{m as __pageData,g as default};
