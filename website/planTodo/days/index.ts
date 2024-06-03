e92285a0a12df18d3792c818d86fef1fed0764c3
grantguo <810153274@qq.com>
2024-06-03 21:08:34 +0800

    add month6

--- website/planTodo/days/index.ts
+++ website/planTodo/days/index.ts
@@ -1,1 +1,2 @@
 export * from './month5'
+export * from './month6'
---
+++ website/planTodo/days/month6.ts
@@ -0,0 +1,39 @@
+export const month6 = [
+  {
+    date: '2024-06-02',
+    child: [
+      {
+        name: "掌握 安卓 和 IOS 应用的发布流程。",
+        done: false,
+        status: 'will'
+      },
+      {
+        name: "掌握 安卓 和 IOS 应用的发布流程。",
+        done: true,
+        status: 'done',
+        rate: '70%'
+      },
+      {
+        name: "学习vue2源码 compiler。",
+        done: false,
+        status: 'will'
+      },
+      {
+        name: "学习vue2源码 compiler。",
+        done: true,
+        status: 'done',
+        rate: '50%'
+      },
+    ]
+  },
+  {
+    date: '2024-06-03',
+    child: [
+      {
+        name: "学习vue2源码 compiler。",
+        done: false,
+        status: 'will'
+      },
+    ]
+  }
+]
