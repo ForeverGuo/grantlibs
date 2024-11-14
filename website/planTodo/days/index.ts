32e7b031101cf75dc0dc6eec2ec9dec77d4e34e9
grantguo <810153274@qq.com>
2024-11-14 18:59:57 +0800

    add 每日清单

--- website/planTodo/days/index.ts
+++ website/planTodo/days/index.ts
@@ -1,2 +1,3 @@
 export * from './month5'
 export * from './month6'
+export * from './month11'
---
+++ website/planTodo/days/month11.ts
@@ -0,0 +1,48 @@
+export const month11 = [
+  {
+    date: '2024-11-14',
+    child: [
+      {
+        name: "内容后台亚洲版发布。",
+        done: true,
+        status: 'done',
+        rate: '100%'
+      },
+      {
+        name: "电商后台屏蔽左侧菜单栏发布。",
+        done: true,
+        status: 'done',
+        rate: '100%'
+      },
+      {
+        name: "体适能参与数据联调。",
+        done: true,
+        status: 'done',
+        rate: '100%'
+      }
+    ]
+  },
+  {
+    date: '2024-11-13',
+    child: [
+      {
+        name: "电商后台品牌推荐接口改造联调处理。",
+        done: true,
+        status: 'done',
+        rate: '100%'
+      },
+      {
+        name: "内容后台退出课程分析接口联调。",
+        done: true,
+        status: 'done',
+        rate: '100%'
+      },
+      {
+        name: "高百赛事抽奖问题修复。",
+        done: true,
+        status: 'done',
+        rate: '100%'
+      }
+    ]
+  }
+]
