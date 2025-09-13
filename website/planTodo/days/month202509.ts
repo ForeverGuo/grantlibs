f9a469ea0bee9b0c47cf2c0904748d70802dd008
grantguo <810153274@qq.com>
2025-09-13 17:02:33 +0800

    petter >> add RN

---
+++ website/planTodo/days/month202509.ts
@@ -0,0 +1,25 @@
+export const month202509 = [
+  {
+    date: '2025-09-13',
+    child: [
+      {
+        name: "RN项目基建-tabbar路由。",
+        done: true,
+        status: 'done',
+        rate: '100%'
+      },
+      {
+        name: "RN项目基建-stack路由。",
+        done: true,
+        status: 'done',
+        rate: '100%'
+      },
+      {
+        name: "RN项目基建-动态修改UIKitten字体。",
+        done: true,
+        status: 'done',
+        rate: '100%'
+      }
+    ]
+  },
+]
